b805dc9d1a1a5e416d01b2393e4929af40bd5516
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-04-22 09:45:59 +0530

    fixed dowpdown link on homepage

--- urls.js
+++ urls.js
@@ -13,10 +13,16 @@ function clickk() {
     if (n =="grocerieslink2") {
         document.getElementById("Groceries").click();
     }
+    if (n =="grocerieslink3") {
+        document.getElementById("Groceries").click();
+    }
     if (n=="todaysdealslink1") {
         document.getElementById("Today's Deals").click();
     }
     if (n=="todaysdealslink2") {
+        document.getElementById("Today's Deals").click();
+    }
+    if (n=="todaysdealslink3") {
         document.getElementById("Today's Deals").click();
     }
     if (n=="bestsellerslink1") {
@@ -25,10 +31,16 @@ function clickk() {
     if (n=="bestsellerslink2") {
         document.getElementById("Best Sellers").click();
     }
+    if (n=="bestsellerslink3") {
+        document.getElementById("Best Sellers").click();
+    }
     if (n=="electronicslink1") {
         document.getElementById("Electronics").click();
     }
     if (n=="electronicslink2") {
+        document.getElementById("Electronics").click();
+    }
+    if (n=="electronicslink3") {
         document.getElementById("Electronics").click();
     }
     if (n=="clothinglink1") {
@@ -37,16 +49,25 @@ function clickk() {
     if (n=="clothinglink2") {
         document.getElementById("Clothing").click();
     }
+    if (n=="clothinglink3") {
+        document.getElementById("Clothing").click();
+    }
     if (n=="bookslink1") {
         document.getElementById("Books").click();
     }
     if (n=="bookslink2") {
         document.getElementById("Books").click();
     }
+    if (n=="bookslink3") {
+        document.getElementById("Books").click();
+    }
     if (n=="newreleaseslink1") {
         document.getElementById("New Releases").click();
     }
     if (n=="newreleaseslink2") {
+        document.getElementById("New Releases").click();
+    }
+    if (n=="newreleaseslink3") {
         document.getElementById("New Releases").click();
     }
     
